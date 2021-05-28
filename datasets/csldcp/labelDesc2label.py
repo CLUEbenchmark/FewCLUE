b63da1f7e99dea3317ec60d77e0428b6f3256552
b <brightmart@hotmail.com>
2021-05-28 11:20:35 +0800

    update

--- datasets/csldcp/labelDesc2label.py
+++ datasets/csldcp/labelDesc2label.py
@@ -6,7 +6,7 @@ label_des2tag={
 "药学":"药学",
 "教育学":"教育",
 "水利工程":"水利",
-"理论经济学":"经济",
+"理论经济学":"理经",
 "食品科学与工程":"食品",
 "畜牧学/兽医学":"兽医",
 "体育学":"体育",
@@ -29,7 +29,7 @@ label_des2tag={
 "哲学":"哲学",
 "公共卫生与预防医学":"卫生",
 "艺术学":"艺术",
-"农业工程":"农业",
+"农业工程":"农工",
 "船舶与海洋工程":"船舶",
 "计算机科学与技术":"计科",
 "冶金工程":"冶金",
@@ -38,21 +38,21 @@ label_des2tag={
 "纺织科学与工程":"纺织",
 "建筑学":"建筑",
 "环境科学与工程":"环境",
-"公共管理":"管理",
+"公共管理":"公管",
 "数学":"数学",
 "物理学":"物理",
 "林学/林业工程":"林业",
 "心理学":"心理",
 "历史学":"历史",
 "工商管理":"工商",
-"应用经济学":"经济",
+"应用经济学":"应经",
 "中医学/中药学":"中医",
 "天文学":"天文",
 "机械工程":"机械",
 "土木工程":"土木",
 "光学工程":"光学",
 "地理学":"地理",
-"农业资源利用":"农业",
+"农业资源利用":"农资",
 "生物学/生物科学与工程":"生物",
 "兵器科学与技术":"兵器",
 "矿业工程":"矿业",
@@ -68,3 +68,13 @@ label_des2tag={
 "地球物理学":"地球",
 "植物保护":"植物"
 }
+
+label_des2tag_reverse={v:k for k,v in label_des2tag.items()}
+label_twoword_list=[v for k,v in label_des2tag.items()]
+print("label_twoword_list:",label_twoword_list,len(label_twoword_list))
+
+# labeltwoword2index_dict={x:index for index,x in enumerate(label_twoword_list)}
+# print("labeltwoword2index_dict:",labeltwoword2index_dict)
+
+label2index_dict={label_des2tag_reverse[x]:index for index,x in enumerate(label_twoword_list)}
+print("label2index_dict:",label2index_dict,len(label2index_dict))
